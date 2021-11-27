854bce071877c9f89e64e551f4babda99127029d
vamsi2ganagalla <94788706+vamsi2ganagalla@users.noreply.github.com>
2021-11-27 23:46:41 +0530

    combine_two_images

---
+++ combining_two_images.py
@@ -0,0 +1,11 @@
+import cv2
+x=cv2.imread('image_name1.png')
+y=cv2.imread('image_name2.png')
+#for combining we need hstack so lets import numpy for that
+import numpy as np
+final=np.hstack((x,y))
+cv2.imshow('hi',final)
+cv2.waitKey()
+cv2.destroyAllWindows()
+#for saving the combined image
+cv2.imwrite('gogeta.png',final)
